a08a6ee556143f42302dd91d5f5f27f3bdf9b756
Bogdan <bogdan@lits-adminMacBook-Pro.local>
2026-05-28 13:36:28 +0300

    done-traits

---
+++ app/hw/traits.php
@@ -0,0 +1,49 @@
+<?php
+trait Test
+{
+    public function test($value=1){
+        return $value;
+    }
+}
+
+trait trait1
+{
+    public function test($value=1): int{
+        return $value;
+    }
+}
+
+trait trait2
+{
+    public function test($value=2):int {
+        return $value;
+    }
+}
+
+trait trait3
+{
+    public function test($value=3): int {
+        return $value;
+    }
+}
+
+class traintsSum{
+    use trait1 , trait2, trait3 {
+        trait1::test insteadof trait2;
+        trait2::test insteadof trait3;
+        trait2::test as getTest2Value;
+        trait3::test as GetTest3Value;
+    }
+
+    public function getSum(): int
+    {
+        return $this->test() + $this->getTest2Value() + $this->getTest3Value();
+    }
+
+
+}
+$res = new traintsSum();
+var_dump($res->getSum());
+
+
+?>
