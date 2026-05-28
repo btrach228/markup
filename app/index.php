355b438dfbdb142ad315a5073231f0f21485414b
Bogdan <bogdan@lits-adminMacBook-Pro.local>
2026-05-28 13:09:04 +0300

    traits done

--- app/index.php
+++ app/index.php
@@ -1,72 +1,48 @@
 <?php
-//
-//$dsn = 'mysql:host=my_database;dbname=nt_db_php;charset=utf8';
-//$db_user = 'root';
-//$db_pass = 'example';
-//
-//try {
-//    $pdo = new PDO($dsn, $db_user, $db_pass);
-//    // Set error mode to exception for easier debugging
-//    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//    echo "Connected successfully";
-//} catch (\PDOException $e) {
-//    throw new \PDOException($e->getMessage(), (int)$e->getCode());
-//}
-
-//class Cart{
-//    public array $product = [];
-//
-//    public function AddProduct(array $product): static
-//    {
-//        $this->product[] = $product;
-//        return $this;
-//    }
-//
-//    public function getTotal(): int|float
-//    {
-//        $total = 0;
-//        foreach ($this->product as $item)
-//        {
-//            $total += $item['price'];
-//        }
-//        return $total;
-//    }
-//
-//}
-//$cart = new Cart();
-//echo $cart->AddProduct(["id"=>2, "title"=>'Toy', "price"=>100, "quantity"=>1])->getTotal();
-//var_dump($cart->AddProduct(["id"=>2, "title"=>'Toy', "price"=>100, "quantity"=>1])->getTotal());
-//var_dump($cart->AddProduct(["id"=>2, "title"=>'Toy', "price"=>100, "quantity"=>1])->getTotal());
-//var_dump($cart->AddProduct(["id"=>2, "title"=>'Toy', "price"=>100, "quantity"=>1])->getTotal());
-//var_dump($cart->AddProduct(["id"=>2, "title"=>'Toy', "price"=>100, "quantity"=>1])->getTotal());
-//var_dump($cart->AddProduct(["id"=>2, "title"=>'Toy', "price"=>100, "quantity"=>1])->getTotal());
-//var_dump($cart);
-
-class Product{
-    // TO PHP 8
-//    public ?string $name;
-//    public int|float $price; // cents
-
-//    public function __construct(string $name='No name', int $price=0)
-//    {
-//    $this->name = $name;
-//    $this->price = $price;
-//    }
-    //AFTER PHP 8
-    public function __construct
-    (
-        public string $name = 'Candy',
-        public int|float $price = 10000// cents
-    )
-    {
+trait Test
+{
+    public function test($value=1){
+        return $value;
     }
-
-    public function getRealPrice(string $currency   ): string{
-        return "{$currency}     " . $this->price / 100;
-    }
-
 }
 
-$product = new Product();
-var_dump($product);
+trait trait1
+{
+    public function test($value=1): int{
+        return $value;
+    }
+}
+
+trait trait2
+{
+    public function test($value=2):int {
+        return $value;
+    }
+}
+
+trait trait3
+{
+    public function test($value=3): int {
+        return $value;
+    }
+}
+
+class traintsSum{
+    use trait1 , trait2, trait3 {
+        trait1::test insteadof trait2;
+        trait2::test insteadof trait3;
+        trait2::test as getTest2Value;
+        trait3::test as GetTest3Value;
+    }
+
+    public function getSum(): int
+    {
+        return $this->test() + $this->getTest2Value() + $this->getTest3Value();
+    }
+
+
+}
+$res = new traintsSum();
+var_dump($res->getSum());
+
 ?>
