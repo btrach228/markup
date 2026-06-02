e033601210933c15aa48ec526ceca2796306e92b
Bogdan <bogdan@lits-adminMacBook-Pro.local>
2026-06-02 22:05:08 +0300

    inial commit solid

--- app/hw/solid/interface-segregation.php
+++ app/hw/solid/interface-segregation.php
@@ -1,1 +1,22 @@
 <?php
+interface eatBird
+{
+    public function eat();
+}
+
+interface flyBird
+{
+    public function fly();
+}
+
+class Swallow implements eatBird, flyBird
+{
+    public function eat() {}
+    public function fly() {}
+}
+
+class Ostrich implements eatBird, flyBird
+{
+    public function eat() {}
+    public function fly() { /* exception */ }
+}
--- app/hw/solid/single-responsibility.php
+++ app/hw/solid/single-responsibility.php
@@ -1,1 +1,24 @@
 <?php
+//встановлюємо дані або отримуємо
+class Product {
+    public function get($name) {}
+    public function set($name, $value) {}
+
+}
+
+//робота з бд
+class manageProduct
+{
+    public function save() {}
+    public function update() {}
+    public function delete() {}
+
+}
+
+// передаємо дані на фронт наприклад
+class displayProduct
+{
+    public function show() {}
+    public function print() {}
+
+}
